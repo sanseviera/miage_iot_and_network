2a75b70fb386795d0ce269728ec60e9a507d9c9c
BORREANI Théo <theoborreani.toba@gmail.com>
2024-02-26 22:24:43 +0100

    Ajout de la gestion du temps et de différentes vitesses du ventilateur.

--- regul/regul.ino
+++ regul/regul.ino
@@ -3,8 +3,6 @@
 #include <Adafruit_NeoPixel.h>
 #include <ArduinoJson.h>
 
-#define PIN 13
-
 //-------------Structures---------------------    
 struct Information { 
   int chanceFeu; // une valeur en pourcentage
@@ -12,18 +10,31 @@ struct Information {
   float temperature;
   int etatRegulateurTemperature; //  0) refroidi , 1) est éteind , 2) chauffe
   bool alerteEtatLed; // pour faire clignoter la led
+  // Variable pour le TIMER
+  long timerGeneral;
+  long timerBandeLed;
+  long timerCommunication;
+  
 };
-struct Information info = {0 , 0.0 , 0.0 , 0, 0};
+struct Information info = {0 , 0.0 , 0.0 , 0, 0, 0.0, 0.0, 0.0};
 
 struct Parametre{
-  const int temperatureSeuilHaut = 27;
-  const int temperatureSeuilBas = 26;
+  const int temperatureSeuilHaut = 25;
+  const int temperatureSeuilBas = 22;
+  const int lumiereAlerte = 3000;
+  const float temperatureAlerte = 20;
+  const int pourcentageAvantAlerte = 80; // Pourcentage à atteindre pour déclencher l'alerte.
   //----------------------------
   const int ledPin = 19;
   const int ledPinVerte = 16;
   const int ledPinJaune = 2;
   const int bandeLedPin = 5;
   const int brocheVentilateur = 26;
+  const int brocheBande = 13;
+  //----------------------------
+  const double periodeTimerGeneral = 1000;
+  const double periodeTimerBandeLed = 300;
+  const double periodeTimerCommunication = 5000;
 };
 struct Parametre parametre = {};
 
@@ -33,7 +44,7 @@ struct Parametre parametre = {};
 OneWire oneWire(23);
 DallasTemperature tempSensor(&oneWire);
 int numberKeyPresses = 0;
-Adafruit_NeoPixel strip(parametre.bandeLedPin, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip(parametre.bandeLedPin, parametre.brocheBande, NEO_GRB + NEO_KHZ800);
 
 void IRAM_ATTR isr() { // Interrupt Handler
   numberKeyPresses++;
@@ -71,15 +82,16 @@ void setChauffage(){
 void setVentilo(){
   if(info.etatRegulateurTemperature == 0){
     digitalWrite(parametre.ledPinVerte, HIGH);
-    analogWrite(parametre.brocheVentilateur, 128);
+    analogWrite(parametre.brocheVentilateur, info.temperature * 4);
   } else{
     digitalWrite(parametre.ledPinVerte, LOW);
     analogWrite(parametre.brocheVentilateur, 0);
   }
 }
 
+
 void setAlerte(){
-  if(info.chanceFeu > 80 ){
+  if(info.chanceFeu >= parametre.pourcentageAvantAlerte ){
     digitalWrite(parametre.ledPinJaune, HIGH);
   } else{
     digitalWrite(parametre.ledPinJaune, LOW);
@@ -180,14 +192,16 @@ void setLed(){
   }
 
 
-      delay(5);
 
     strip.show();
 }
 
-
+/* 
+* Fonction qui permet d'augmenter ou de réduire la variable représentant
+* le pourcentage de chances d'incendies.
+*/
 void setDetectorFire(){
-  if(info.lumiere > 3000 && info.temperature > 20){
+  if(info.lumiere > parametre.lumiereAlerte && info.temperature > parametre.temperatureAlerte){
     info.chanceFeu = info.chanceFeu+10;
   }
   else{
@@ -237,19 +251,32 @@ void setup(){
   initCapteurChaleur();
   initVentilo();
   setBandeLed();
+  delay(1000);
 }
 
 void loop() {
-  info.lumiere = lireCapteurLumiere();
-  info.temperature = lireCapteurChaleur();
-  setDetectorFire();
-  setEtatRegulateurTemperature();
-  setChauffage();
-  setVentilo();
-  setLed();
-  setAlerte();
-  informationPrint();
-  makeJSON();
-  delay(1000);
+  // Ce type de condition permet d'exécuter le contenu toutes les N millisecondes, ce qui évite d'avoir un programme trop linéaire.
+  if(info.timerGeneral == 0 || millis() - info.timerGeneral > parametre.periodeTimerGeneral){
+
+    info.timerGeneral=millis();
+    info.lumiere = lireCapteurLumiere();
+    info.temperature = lireCapteurChaleur();
+
+    setDetectorFire();
+    setEtatRegulateurTemperature();
+    setChauffage();
+    setVentilo();
+    setAlerte();
+  }
+  if(info.timerBandeLed == 0 || millis() - info.timerBandeLed > parametre.periodeTimerBandeLed){
+    info.timerBandeLed=millis();
+    setLed();
+  }
+  if(info.timerCommunication == 0 || millis() - info.timerCommunication > parametre.periodeTimerCommunication){
+    info.timerCommunication=millis();
+    informationPrint();
+    makeJSON();
+  }
+  
 
 }
